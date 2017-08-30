7b357fe89cc92a73d145983edfc3783c9116ecc1
terrenceliu <yl124@rice.edu>
2017-08-29 23:15:14 -0500

    add herkimer mollusk

--- source.py
+++ source.py
@@ -37,7 +37,7 @@ def test():
 	with open('template.html', 'r') as f:
 		tmp = Template(f.read())
 	
-	sec_title = ['SF', 'SH', 'NK', 'Uttar Pradesh']
+	sec_title = ['SF', 'SH', 'NK', 'Uttar Pradesh', 'Herkimer Mollusk']
 	pg_title = [i.lower().replace(' ', '_') for i in sec_title]
 	secs = []
 	
